17a752150d0af28213a0cc6fc17f261066812f17
Abe Voelker <abe@abevoelker.com>
2012-03-12 23:21:45 -0500

    Add some code comments to OE driver def

--- do_openedge/ext-java/src/main/java/do_openedge/OpenEdgeDriverDefinition.java
+++ do_openedge/ext-java/src/main/java/do_openedge/OpenEdgeDriverDefinition.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
 import java.util.Properties;
+import java.net.URI;
 
 import org.jruby.runtime.builtin.IRubyObject;
 
@@ -13,8 +14,9 @@ import data_objects.RubyType;
 import data_objects.drivers.AbstractDriverDefinition;
 
 public class OpenEdgeDriverDefinition extends AbstractDriverDefinition {
+
     public final static String URI_SCHEME = "openedge";
-    // . will be replaced with : in Connection.java before connection
+    // . in JDBC_URI_SCHEME will be replaced with : in Connection.java before connection
     public final static String JDBC_URI_SCHEME = "datadirect.openedge";
     public final static String RUBY_MODULE_NAME = "Openedge";
     public final static String JDBC_DRIVER = "com.ddtek.jdbc.openedge.OpenEdgeDriver";
@@ -23,22 +25,29 @@ public class OpenEdgeDriverDefinition extends AbstractDriverDefinition {
      *
      */
     public OpenEdgeDriverDefinition() {
-        //super(URI_SCHEME, RUBY_MODULE_NAME, JDBC_DRIVER);
         super(URI_SCHEME, JDBC_URI_SCHEME, RUBY_MODULE_NAME, JDBC_DRIVER);
     }
 
     /**
+     *
+     * Verified by checking that
+     * conn.getMetaData().supportsGetGeneratedKeys() == false
+     * and confirmed experimentally - when calling conn.prepareStatement with
+     * Statement.RETURN_GENERATED_KEYS, an error is thrown:
+     * java.sql.SQLFeatureNotSupportedException
      *
      * @return
      */
     @Override
     public boolean supportsJdbcGeneratedKeys()
     {
-        //javaConn.getMetaData().supportsGetGeneratedKeys()
         return false;
     }
 
     /**
+     *
+     * Verified by successfully calling conn.prepareStatement with
+     * Statement.NO_GENERATED_KEYS
      *
      * @return
      */
@@ -48,20 +57,21 @@ public class OpenEdgeDriverDefinition extends AbstractDriverDefinition {
 
     /**
      *
+     * TODO - see how Oracle driver does this w/ its sequences
+     *
      * @param connection
      * @return
      */
     @Override
     public ResultSet getGeneratedKeys(Connection connection) {
-        try {
-            return connection.prepareStatement("CALL IDENTITY()").executeQuery();
-        } catch (SQLException ex) {
-            return null;
-        }
+        return null;
     }
 
-    // TODO: Verify this is true
+
     /**
+     *
+     * Verified by creating a statement with ResultSet.TYPE_SCROLL_INSENSITIVE
+     * and successfully scrolling forward (rs.next()) *and backward* (rs.previous())!
      *
      * @return
      */
@@ -72,6 +82,8 @@ public class OpenEdgeDriverDefinition extends AbstractDriverDefinition {
 
     /**
      *
+     * TODO
+     *
      * @return
      */
     @Override
@@ -81,6 +93,28 @@ public class OpenEdgeDriverDefinition extends AbstractDriverDefinition {
         props.put("databaseName", "test2012");
         props.put("user", "Abe");
         return props;
+    }
+
+    /**
+     *
+     * TODO
+     *
+     * @param connectionUri
+     * @return
+     */
+    @Override
+    public String getJdbcUri(URI connectionUri) {
+      String jdbcUri = connectionUri.toString();
+      System.out.println("JDBC STARTING URI IS " + jdbcUri);
+
+      // Replace . with : in scheme name - necessary for OpenEdge scheme datadirect:openedge
+      // : cannot be used in JDBC_URI_SCHEME as then it is identified as opaque URI
+      jdbcUri = jdbcUri.replaceFirst("^([a-z]+)(\\.)", "$1:");
+
+      if (!jdbcUri.startsWith("jdbc:")) {
+          jdbcUri = "jdbc:" + jdbcUri;
+      }
+      return jdbcUri;
     }
 
 }
